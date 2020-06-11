71b31aea72e008e1072c667225a10f508977bdd0
Eric Bartholemy <eric@kabisa.nl>
2020-06-12 01:41:00 +0200

    Revert "test"
    This reverts commit 288c35d56723c5599d62597254fc8a5104a8bf87.

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,5 +33,6 @@ module.exports = {
     "gatsby-plugin-netlify",
     "gatsby-plugin-catch-links",
   ],
-  pathPrefix: "/",
+  pathPrefix: "/curriculum-vitae",
+  assetPrefix: "https://thnukid.github.io/curriculum-vitae",
 }
